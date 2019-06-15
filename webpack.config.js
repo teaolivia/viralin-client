c3e05e69b9e61bc8c8b8f75bcee26013bd968149
Theaolivia <theaolivia@Theaolivias-iMac.local>
2019-06-15 20:07:43 +0700

    Issues still not solved but I have changed webpack config and compile successful

--- webpack.config.js
+++ webpack.config.js
@@ -59,7 +59,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, './public'),
     publicPath: '/',
-    filename: 'bundle.js'
+    filename: 'index.js'
   },
   devServer: {
     contentBase: "./build"
