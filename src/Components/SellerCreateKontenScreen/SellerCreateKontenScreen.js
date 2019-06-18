55251994371a0df5a1760a7d292d2cbf94325565
Mahdiar Naufal <mahdiarnaufal@gmail.com>
2019-06-19 04:41:22 +0700

    SellerCreateKontenScreen Components

---
+++ src/Components/SellerCreateKontenScreen/SellerCreateKontenScreen.js
@@ -0,0 +1,22 @@
+/* eslint-disable import/no-unresolved */
+/* eslint-disable import/no-extraneous-dependencies */
+import React from 'react';
+
+import 'Components/SellerCreateKontenScreen/SellerCreateKontenScreen.css';
+
+class SellerCreateKontenScreen extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div className="SellerCreateKontenScreen">
+        a
+      </div>
+    );
+  }
+}
+
+export default SellerCreateKontenScreen;
