7f9f1c1b2392315ea3b1381f285885fefe840b45
Theaolivia <theaolivia@Theaolivias-iMac.local>
2019-06-19 05:08:30 +0700

    add and delete data

--- src/Models/models.js
+++ src/Models/models.js
@@ -2,3 +2,22 @@
 // ========================================================
 // 
 // https://my-json-server.typicode.com/teaolivia/viralin-db
+import './db'
+
+function addItem(db, item) {
+    var res = JSON.parse(db);
+    db['Items'].push(item);
+    res = JSON.stringify(obj);
+}
+
+function deleteItem(idx) {
+    delete db.Items[idx];
+}
+
+function updateItem() {
+
+}
+
+function getItem() {
+
+}
