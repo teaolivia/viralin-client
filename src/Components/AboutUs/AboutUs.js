a334c51bb6b0556d8c76046ce1106a7617736b15
Mahdiar Naufal <mahdiarnaufal@gmail.com>
2019-06-24 22:04:40 +0700

    add link to twitter, facebook, and instagram

--- src/Components/AboutUs/AboutUs.js
+++ src/Components/AboutUs/AboutUs.js
@@ -6,31 +6,15 @@ import 'react-responsive-carousel/lib/styles/carousel.min.css';
 import Grid from '@material-ui/core/Grid';
 import SvgIcon from '@material-ui/core/SvgIcon';
 import Typography from '@material-ui/core/Typography';
-import Card from '@material-ui/core/Card';
-import CardHeader from '@material-ui/core/CardHeader';
-import CardMedia from '@material-ui/core/CardMedia';
-import CardContent from '@material-ui/core/CardContent';
-import Avatar from '@material-ui/core/Avatar';
 
 import Email from '@material-ui/icons/Email';
 import Phone from '@material-ui/icons/Phone';
 import Domain from '@material-ui/icons/Domain';
 
-import Settings from '@material-ui/icons/Settings';
-import Check from '@material-ui/icons/Check';
-import AttachMoney from '@material-ui/icons/AttachMoney';
-import Share from '@material-ui/icons/Share';
-import CardGiftcard from '@material-ui/icons/CardGiftcard';
-
-import CountUp from 'react-countup';
-import OnVisible from 'react-on-visible';
-
 import telkom from 'images/telkom.png';
 import logo1 from 'images/logo1.png';
 import logo2 from 'images/logo2.png';
 import about from 'images/about.png';
-import howitworks from 'images/howitworks.png';
-import howitworks2 from 'images/howitworks2.jpg';
 
 import 'Components/AboutUs/AboutUs.css';
 
@@ -81,7 +65,7 @@ class Home extends React.Component {
             <Grid
               item
             >
-              <Link className="url" to="/">
+              <a className="url" href="https://www.instagram.com/goviralin">
                 <SvgIcon
                   viewBox="0 0 24 24"
                 >
@@ -89,12 +73,12 @@ class Home extends React.Component {
                     <path fill="#fff" d="M7.8,2H16.2C19.4,2 22,4.6 22,7.8V16.2A5.8,5.8 0 0,1 16.2,22H7.8C4.6,22 2,19.4 2,16.2V7.8A5.8,5.8 0 0,1 7.8,2M7.6,4A3.6,3.6 0 0,0 4,7.6V16.4C4,18.39 5.61,20 7.6,20H16.4A3.6,3.6 0 0,0 20,16.4V7.6C20,5.61 18.39,4 16.4,4H7.6M17.25,5.5A1.25,1.25 0 0,1 18.5,6.75A1.25,1.25 0 0,1 17.25,8A1.25,1.25 0 0,1 16,6.75A1.25,1.25 0 0,1 17.25,5.5M12,7A5,5 0 0,1 17,12A5,5 0 0,1 12,17A5,5 0 0,1 7,12A5,5 0 0,1 12,7M12,9A3,3 0 0,0 9,12A3,3 0 0,0 12,15A3,3 0 0,0 15,12A3,3 0 0,0 12,9Z" />
                   </svg>
                 </SvgIcon>
-              </Link>
+              </a>
             </Grid>
             <Grid
               item
             >
-              <Link className="url" to="/">
+              <a className="url" href="https://twitter.com/goviralin">
                 <SvgIcon
                   viewBox="0 0 24 24"
                 >
@@ -102,12 +86,12 @@ class Home extends React.Component {
                     <path fill="#fff" d="M22.46,6C21.69,6.35 20.86,6.58 20,6.69C20.88,6.16 21.56,5.32 21.88,4.31C21.05,4.81 20.13,5.16 19.16,5.36C18.37,4.5 17.26,4 16,4C13.65,4 11.73,5.92 11.73,8.29C11.73,8.63 11.77,8.96 11.84,9.27C8.28,9.09 5.11,7.38 3,4.79C2.63,5.42 2.42,6.16 2.42,6.94C2.42,8.43 3.17,9.75 4.33,10.5C3.62,10.5 2.96,10.3 2.38,10C2.38,10 2.38,10 2.38,10.03C2.38,12.11 3.86,13.85 5.82,14.24C5.46,14.34 5.08,14.39 4.69,14.39C4.42,14.39 4.15,14.36 3.89,14.31C4.43,16 6,17.26 7.89,17.29C6.43,18.45 4.58,19.13 2.56,19.13C2.22,19.13 1.88,19.11 1.54,19.07C3.44,20.29 5.7,21 8.12,21C16,21 20.33,14.46 20.33,8.79C20.33,8.6 20.33,8.42 20.32,8.23C21.16,7.63 21.88,6.87 22.46,6Z" />
                   </svg>
                 </SvgIcon>
-              </Link>
+              </a>
             </Grid>
             <Grid
               item
             >
-              <Link className="url" to="/">
+              <a className="url" href="https://www.instagram.com/goviralin">
                 <SvgIcon
                   viewBox="0 0 24 24"
                 >
@@ -115,7 +99,7 @@ class Home extends React.Component {
                     <path fill="#fff" d="M17,2V2H17V6H15C14.31,6 14,6.81 14,7.5V10H14L17,10V14H14V22H10V14H7V10H10V6A4,4 0 0,1 14,2H17Z" />
                   </svg>
                 </SvgIcon>
-              </Link>
+              </a>
             </Grid>
           </Grid>
         </Grid>
@@ -340,7 +324,7 @@ class Home extends React.Component {
                   </Typography>
                 </Grid>
                 <Grid item md={12}>
-                  <Link to="/twitter" className="footerLinks">
+                  <Link to="/facebook" className="footerLinks">
                     <Typography variant="subtitle1">
                       <SvgIcon
                         viewBox="0 0 24 24"
@@ -349,12 +333,12 @@ class Home extends React.Component {
                           <path fill="#fff" d="M17,2V2H17V6H15C14.31,6 14,6.81 14,7.5V10H14L17,10V14H14V22H10V14H7V10H10V6A4,4 0 0,1 14,2H17Z" />
                         </svg>
                       </SvgIcon>
-                      Viralin
+                      viralin Bisnismu
                     </Typography>
                   </Link>
                 </Grid>
                 <Grid item md={12}>
-                  <Link to="/twitter" className="footerLinks">
+                  <a href="https://twitter.com/goviralin" className="footerLinks">
                     <Typography variant="subtitle1">
                       <SvgIcon
                         viewBox="0 0 24 24"
@@ -363,46 +347,12 @@ class Home extends React.Component {
                           <path fill="#fff" d="M22.46,6C21.69,6.35 20.86,6.58 20,6.69C20.88,6.16 21.56,5.32 21.88,4.31C21.05,4.81 20.13,5.16 19.16,5.36C18.37,4.5 17.26,4 16,4C13.65,4 11.73,5.92 11.73,8.29C11.73,8.63 11.77,8.96 11.84,9.27C8.28,9.09 5.11,7.38 3,4.79C2.63,5.42 2.42,6.16 2.42,6.94C2.42,8.43 3.17,9.75 4.33,10.5C3.62,10.5 2.96,10.3 2.38,10C2.38,10 2.38,10 2.38,10.03C2.38,12.11 3.86,13.85 5.82,14.24C5.46,14.34 5.08,14.39 4.69,14.39C4.42,14.39 4.15,14.36 3.89,14.31C4.43,16 6,17.26 7.89,17.29C6.43,18.45 4.58,19.13 2.56,19.13C2.22,19.13 1.88,19.11 1.54,19.07C3.44,20.29 5.7,21 8.12,21C16,21 20.33,14.46 20.33,8.79C20.33,8.6 20.33,8.42 20.32,8.23C21.16,7.63 21.88,6.87 22.46,6Z" />
                         </svg>
                       </SvgIcon>
-                      myviralin
+                      goviralin
                     </Typography>
-                  </Link>
-                </Grid>
-              </Grid>
-              <Grid
-                item
-                md={3}
-                container
-              >
-                <Grid item md={12}>
-                  <Link to="/twitter" className="footerLinks">
-                    <Typography variant="subtitle1">
-                      <SvgIcon
-                        viewBox="0 0 24 24"
-                      >
-                        <svg>
-                          <path fill="#fff" d="M17,2V2H17V6H15C14.31,6 14,6.81 14,7.5V10H14L17,10V14H14V22H10V14H7V10H10V6A4,4 0 0,1 14,2H17Z" />
-                        </svg>
-                      </SvgIcon>
-                      Viralin
-                    </Typography>
-                  </Link>
+                  </a>
                 </Grid>
                 <Grid item md={12}>
-                  <Link to="/twitter" className="footerLinks">
-                    <Typography variant="subtitle1">
-                      <SvgIcon
-                        viewBox="0 0 24 24"
-                      >
-                        <svg>
-                          <path fill="#fff" d="M22.46,6C21.69,6.35 20.86,6.58 20,6.69C20.88,6.16 21.56,5.32 21.88,4.31C21.05,4.81 20.13,5.16 19.16,5.36C18.37,4.5 17.26,4 16,4C13.65,4 11.73,5.92 11.73,8.29C11.73,8.63 11.77,8.96 11.84,9.27C8.28,9.09 5.11,7.38 3,4.79C2.63,5.42 2.42,6.16 2.42,6.94C2.42,8.43 3.17,9.75 4.33,10.5C3.62,10.5 2.96,10.3 2.38,10C2.38,10 2.38,10 2.38,10.03C2.38,12.11 3.86,13.85 5.82,14.24C5.46,14.34 5.08,14.39 4.69,14.39C4.42,14.39 4.15,14.36 3.89,14.31C4.43,16 6,17.26 7.89,17.29C6.43,18.45 4.58,19.13 2.56,19.13C2.22,19.13 1.88,19.11 1.54,19.07C3.44,20.29 5.7,21 8.12,21C16,21 20.33,14.46 20.33,8.79C20.33,8.6 20.33,8.42 20.32,8.23C21.16,7.63 21.88,6.87 22.46,6Z" />
-                        </svg>
-                      </SvgIcon>
-                      myviralin
-                    </Typography>
-                  </Link>
-                </Grid>
-                <Grid item md={12}>
-                  <Link to="/twitter" className="footerLinks">
+                  <a href="https://www.instagram.com/goviralin" className="footerLinks">
                     <Typography variant="subtitle1">
                       <SvgIcon
                         viewBox="0 0 24 24"
@@ -411,24 +361,17 @@ class Home extends React.Component {
                           <path fill="#fff" d="M7.8,2H16.2C19.4,2 22,4.6 22,7.8V16.2A5.8,5.8 0 0,1 16.2,22H7.8C4.6,22 2,19.4 2,16.2V7.8A5.8,5.8 0 0,1 7.8,2M7.6,4A3.6,3.6 0 0,0 4,7.6V16.4C4,18.39 5.61,20 7.6,20H16.4A3.6,3.6 0 0,0 20,16.4V7.6C20,5.61 18.39,4 16.4,4H7.6M17.25,5.5A1.25,1.25 0 0,1 18.5,6.75A1.25,1.25 0 0,1 17.25,8A1.25,1.25 0 0,1 16,6.75A1.25,1.25 0 0,1 17.25,5.5M12,7A5,5 0 0,1 17,12A5,5 0 0,1 12,17A5,5 0 0,1 7,12A5,5 0 0,1 12,7M12,9A3,3 0 0,0 9,12A3,3 0 0,0 12,15A3,3 0 0,0 15,12A3,3 0 0,0 12,9Z" />
                         </svg>
                       </SvgIcon>
-                      myviralin
+                      goviralin
                     </Typography>
-                  </Link>
+                  </a>
                 </Grid>
-                <Grid item md={12}>
-                  <Link to="/twitter" className="footerLinks">
-                    <Typography variant="subtitle1">
-                      <SvgIcon
-                        viewBox="0 0 24 24"
-                      >
-                        <svg>
-                          <path fill="#fff" d="M10,15L15.19,12L10,9V15M21.56,7.17C21.69,7.64 21.78,8.27 21.84,9.07C21.91,9.87 21.94,10.56 21.94,11.16L22,12C22,14.19 21.84,15.8 21.56,16.83C21.31,17.73 20.73,18.31 19.83,18.56C19.36,18.69 18.5,18.78 17.18,18.84C15.88,18.91 14.69,18.94 13.59,18.94L12,19C7.81,19 5.2,18.84 4.17,18.56C3.27,18.31 2.69,17.73 2.44,16.83C2.31,16.36 2.22,15.73 2.16,14.93C2.09,14.13 2.06,13.44 2.06,12.84L2,12C2,9.81 2.16,8.2 2.44,7.17C2.69,6.27 3.27,5.69 4.17,5.44C4.64,5.31 5.5,5.22 6.82,5.16C8.12,5.09 9.31,5.06 10.41,5.06L12,5C16.19,5 18.8,5.16 19.83,5.44C20.73,5.69 21.31,6.27 21.56,7.17Z" />
-                        </svg>
-                      </SvgIcon>
-                      Viralin
-                    </Typography>
-                  </Link>
-                </Grid>
+              </Grid>
+              <Grid
+                item
+                md={3}
+                container
+              >
+                &nbsp;
               </Grid>
               <Grid
                 item
